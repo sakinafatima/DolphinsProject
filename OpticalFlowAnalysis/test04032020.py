8a1d33ccd6727bb11852c9b77b11fe339f0035ec
SakinaFatima <sakina725@gmail.com>
2020-03-11 18:47:30 +0000

    Update test04032020.py

--- OpticalFlowAnalysis/test04032020.py
+++ OpticalFlowAnalysis/test04032020.py
@@ -5,82 +5,113 @@ import cv2 as cv
 from random import randint
 import matplotlib.pyplot as plt
 
+from ocr import getMagnification
+
+font = cv.FONT_HERSHEY_SIMPLEX
 lk_params = dict(winSize=(15, 15),
                  maxLevel=2,
                  criteria=(cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
 
 fgbg = cv.createBackgroundSubtractorMOG2()
 contourpoints=[]
+box=[]
 
 cap = cv.VideoCapture('test3.mp4')
 ret, frame = cap.read()
-frame = frame[130:1030, 0:1990]
 
-lin = np.zeros_like(frame)
-
+#frame = frame[130:1030, 0:1990]
 while cap.isOpened():
     ret, frame2 = cap.read()
-    frame2 = frame2[130:1030, 0:1990]
+    magnification = getMagnification(frame2)
+    print("magnification value", magnification)
+    #frame = frame[130:1030, 0:1990]
+    #frame2 = frame2[130:1030, 0:1990]
     frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     fgmask = fgbg.apply(frame_gray)
-    vis = frame.copy()
-    vis1 = frame.copy()
+    vis = frame2.copy()
     blur = cv.GaussianBlur(fgmask, (5, 5), 0)
     _, thresh = cv.threshold(blur, 20, 255, cv.THRESH_BINARY)
     dilated = cv.dilate(thresh, None, iterations=3)
     # finding contours of each pixel that is moving within the frame
     _, contours, _ = cv.findContours(dilated, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    cv.drawContours(vis1, contours, -1, (0, 255, 0), 2)
+    cv.drawContours(vis, contours, -1, (0, 255, 0), 2)
 
+    # for i in range(0, len(contours)):
+    #     cnt = contours[i]
+    #     cnt = cnt.astype(np.float32)
     for i in range(0, len(contours)):
-        cnt = contours[i]
-        cnt = cnt.astype(np.float32)
-        for i in range(0, len(contours)):
             cnt = contours[i]
-            cnt = cnt.astype(np.float32)
-            x, y, w, h = cv.boundingRect(cnt)
+            #cnt = cnt.astype(np.float32)
+            box.append(cv.boundingRect(cnt))
+            x, y, w, h = box[i]
             cx = x + (w / 2)
             cy = y + (h / 2)
-            contourpoints.append([x, y])
-            contourpoints.append([x + w, y + h])
-            contourpoints.append([x + w, y])
-            contourpoints.append([x, y + h])
             contourpoints.append([cx, cy])
+            # contourpoints.append([x + w, y + h])
+            # contourpoints.append([x + w, y])
+            # contourpoints.append([x, y + h])
+           # print("contourpoints in loop------", contourpoints)
     new_gray = cv.cvtColor(frame2, cv.COLOR_BGR2GRAY)
-
+    #print("contourpoints",contourpoints)
     p0 = np.array(contourpoints, np.float32)
-    p1, _st, _err = cv.calcOpticalFlowPyrLK(frame_gray, new_gray, p0, None, **lk_params)
-    #see to the status array: _st: contains status of the optical flow displacement, if st==1 means there is change in displacement
-    print("fitting kmeans")
-    kmeans =KMeans(n_clusters=3, random_state=0).fit(p1)
-    print("k means complete")
-    #labels = kmeans.labels_
-    print("Kmeans Labels------",kmeans.labels_)
-    for i, (f2, f1) in enumerate(zip(p1, p0)): #list comprehension
-            a, b = f2.ravel()
-            c, d = f1.ravel()
-            label = kmeans.predict([[a, b]])
-            if (label == 0):
-                #yellow
-               vis = cv.circle(vis, (a, b), 4, (0, 0, 0), -1)
-              # vis= cv.rectangle(vis, (a, b), (a + c, b + d), (0, 255, 0), 2)
-            elif (label == 1):
-                # pink
-                vis = cv.circle(vis, (a, b), 4, (255, 0, 255), -1)
-               # vis = cv.rectangle(vis, (a, b), (c, d), (0, 255, 0), 2)
-            elif (label == 2):
-                #purple
-                vis = cv.circle(vis, (a, b), 4, (128, 128, 255), -1)
-            # lin = cv.circle(lin, (c, d), 4, (255, 0, 0), -1) #red color
+    print("contourpoints after np array------",p0, len(p0))
+    if len(p0)>0:
+        p1, _st, _err = cv.calcOpticalFlowPyrLK(frame_gray, new_gray, p0, None, **lk_params)
+        #print ("result of optical flow",p1)
+        #see to the status array: _st: contains status of the optical flow displacement, if st==1 means there is change in displacement
+        print("fitting kmeans")
+        if len(p0)<=2:
+            #random_state makes result reproduceable trying in making k-mean optimezed. as results of kmean is stochastic
+         kmeans =KMeans(n_clusters=1, random_state=0).fit(p1)
+        else:
+         kmeans = KMeans(n_clusters=3, random_state=0).fit(p1)
+        print("k means complete")
+        #labels = kmeans.labels_
+        print("Kmeans Labels------",kmeans.labels_)
+        for i, (f2, f1) in enumerate(zip(p1, p0)): #list comprehension
+                a, b = f2.ravel()
+                c, d = f1.ravel()
+                label = kmeans.predict([[a, b]])
+                if (label==0):
+                        #black
+                     #vis = cv.circle(vis, (a, b), 2, (0, 0, 0), -1)
+                     x, y, w, h= box[i]
+                     if magnification<=4.0:
+                      if(w<50 and h<50):
 
-    print("p1 and p0r", p0)
+                         vis=cv.rectangle(vis, (x,y),(x+w,y+h), (0, 0, 0),2)
+                      elif (magnification>4.0):
+                         if (w > 80 and h > 80):
+                          vis = cv.rectangle(vis, (x, y), (x + w, y + h), (0, 0, 0), 2)
+                elif (label == 1):
+                        # pink
+                        #vis = cv.circle(vis, (a, b), 4, (255, 0, 255), -1)
+                        if magnification <= 4.0:
+                            if (w < 80 and h < 80):
+
+                                vis = cv.rectangle(vis, (x, y), (x + w, y + h), (255, 0, 255), 2)
+                            elif (magnification > 4.0):
+                                if (w > 50 and h > 50):
+                                    vis = cv.rectangle(vis, (x, y), (x + w, y + h), (255, 0, 255), 2)
+                elif (label == 2):
+                        #purple
+                        #vis = cv.circle(vis, (a, b), 4, (128, 128, 255), -1)
+                        if magnification <= 4.0:
+                            if (w < 80 and h < 80):
+
+                                vis = cv.rectangle(vis, (x, y), (x + w, y + h), (128, 128, 255), 2)
+                            elif (magnification > 4.0):
+                                if (w > 80 and h > 50):
+                                    vis = cv.rectangle(vis, (x, y), (x + w, y + h), (128, 128, 255), 2)
+
     #cv.imshow('frame1', vis)
     print("rendering image")
     cv.imshow("image",vis)
-    cv.imshow("image", vis1)
-    # cv.imshow("frame2", fgmask)
-    # cv.imshow("frame", frame)
+    cv.imshow("frame2", fgmask)
+    cv.imshow("frame", frame)
     frame=frame2
+    del contourpoints[:]
+    del box[:]
     if cv.waitKey(40) == ord('q'):
         break
 cv.destroyAllWindows()
